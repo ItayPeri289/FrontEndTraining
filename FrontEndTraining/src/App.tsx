e7b1e4e430451cbc85af65c66f8c54462f831d51
ItayPeri289 <peri.itay289@gmail.com>
2025-04-10 15:53:02 +0300

    added all products with a simple card or each product

--- FrontEndTraining/src/App.tsx
+++ FrontEndTraining/src/App.tsx
@@ -2,11 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import MediaCard from './components/MediaCard'
-import './App.css'
+import './App.css';
+import productsArray from './data/Products.json';
 
 function App() {
- // const [count, setCount] = useState(0)
-  return  <MediaCard imageUrl='src/images/bamba.png' title='Bamba' description='bambaaaaaa'></MediaCard>;
+  return  (
+    <div>
+    {Array.from({length: productsArray.length}).map((_, index) => (
+      <MediaCard imageUrl={productsArray[index].image} description={productsArray[index].description} title={productsArray[index].name}/>
+    ))}
+  </div>
+  );
 }
 
 export default App;
