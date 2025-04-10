c278ae569bce5e0e35839acdb7a52642f3b7e796
ItayPeri289 <peri.itay289@gmail.com>
2025-04-10 16:10:08 +0300

    added flexbox to the tabs

--- FrontEndTraining/src/App.tsx
+++ FrontEndTraining/src/App.tsx
@@ -4,13 +4,18 @@ import viteLogo from '/vite.svg'
 import MediaCard from './components/MediaCard'
 import './App.css';
 import productsArray from './data/Products.json';
+import MenuBar from './components/MenuBar';
 
 function App() {
   return  (
     <div>
+    <MenuBar/>
+    <br/>
+    <div className='tabs'>
     {Array.from({length: productsArray.length}).map((_, index) => (
       <MediaCard imageUrl={productsArray[index].image} description={productsArray[index].description} title={productsArray[index].name}/>
     ))}
+    </div>
   </div>
   );
 }
