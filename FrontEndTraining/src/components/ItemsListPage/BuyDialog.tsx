e7db349f6c30318af28b8cf18127686e87956ed8
ItayPeri289 <peri.itay289@gmail.com>
2025-05-15 15:50:37 +0300

    removed imports from buydialog

--- FrontEndTraining/src/components/ItemsListPage/BuyDialog.tsx
+++ FrontEndTraining/src/components/ItemsListPage/BuyDialog.tsx
@@ -1,10 +1,8 @@
-import * as React from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
-import { colors } from "@mui/material";
 
 interface handleThings {
   handleClose: () => void;
